9bc1472dad95665a6fe4c281593a2e9eb5322984
zhouailiang <zhou1475943445@163.com>
2026-01-12 13:51:52 +0800

    Site updated: 2026-01-12 13:51:52

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/3eeb.html","posts/0.html","posts/1.html"];function toRandomPost(){
+var posts=["posts/0.html","posts/3eeb.html","posts/1.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
