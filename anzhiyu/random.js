d9d948daa34256151a1fc2225d574cce28dccecc
zhouailiang <zhou1475943445@126.com>
2026-01-13 10:53:30 +0800

    Site updated: 2026-01-13 10:53:30

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/1.html","posts/123c.html","posts/10219.html","posts/7737.html","posts/38054.html","posts/13178.html","posts/0.html","posts/64332.html"];function toRandomPost(){
+var posts=["posts/123c.html","posts/1.html","posts/38054.html","posts/7737.html","posts/0.html","posts/13178.html","posts/64332.html","posts/10219.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
